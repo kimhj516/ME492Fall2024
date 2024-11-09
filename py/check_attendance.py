6739ee9e7c492eee07eb7e9c73ff112c3a80adf7
Emotionchord <jsh99@kaist.ac.kr>
2024-11-09 18:33:58 +0900

    jsh99 prob 2-b

--- py/check_attendance.py
+++ py/check_attendance.py
@@ -6,3 +6,9 @@ with open('student_data.json', 'r') as file:
 
 for each_student in data["student_data"]:
     print('student name: ' + each_student["name"] + ', email address: ' + each_student["email"] + ', github id: ' + each_student["github_id"])
+
+def jsh99():
+    # A function which prints student ID for every student.
+    # Since there are no student ID info in json file yet, so this function won't work properly.
+    for each_student in data["student_data"]:
+        print(each_student["name"], "'s student ID is ", each_student["student_id"])
