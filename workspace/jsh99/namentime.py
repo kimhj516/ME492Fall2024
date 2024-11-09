b39d846f33f15661fbf877e70e81ea72f529754e
Emotionchord <jsh99@kaist.ac.kr>
2024-11-09 18:02:12 +0900

    jsh99 prob 1

---
+++ workspace/jsh99/namentime.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+name = "Seo Hyeon Jeong"
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+print(f"My name is {name} and the current time is {current_time}")
+
