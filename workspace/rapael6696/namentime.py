4b3dafcd9d18d612cc74d7d9fe3cf74899798e45
Kyeonghun Lee <rapael6696@kaist.ac.kr>
2024-11-12 01:35:24 +0900

    Add namentime.py for name and current time output

---
+++ workspace/rapael6696/namentime.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+name = "Kyeonghun Lee"
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+print(f"My name is {name}. The current time information - {current_time}")
+
