a255373825e986d1127ae444c2b585a710196149
kwun minjae <kopgh8026@kaist.ac.kr>
2024-11-14 13:55:04 +0900

    make work space

---
+++ workspace/kopgh8026/nametime.py
@@ -0,0 +1,9 @@
+import datetime
+
+def print_name_and_time():
+    name = "Kwun minjae"
+    current_time = datetime.datetime.now()
+    print(f"Hello, my name is {name}. Current time is: {current_time}")
+
+if __name__ == "__main__":
+    print_name_and_time()
