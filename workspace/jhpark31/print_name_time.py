4727cc0a6e7fa0ad8462082447cfaf45617f595d
jhpark31 <jhpark31@kaist.ac.kr>
2024-11-15 19:59:59 +0900

    Add print_name_time.py to display name and current time

---
+++ workspace/jhpark31/print_name_time.py
@@ -0,0 +1,9 @@
+import datetime
+
+def print_name_time():
+    name = "JUNHYEOK PARK"
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Name: {name}")
+    print(f"Current Time: {Current_time}")
+
+print_name_and_time()
