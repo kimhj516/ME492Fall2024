dfb25058f29375ff7935105a9d1f1b9e16272133
leohyun777 <leohyun777@kaist.ac.kr>
2024-11-15 13:42:05 +0900

    HW3-1 submit

--- workspace/leohyun777/name_time.py
+++ workspace/leohyun777/name_time.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
 name = "Jaehyun Kim"
-current_time = datetime.datetime.now()
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 print(f"Name : {name}, Current time: {current_time}")
