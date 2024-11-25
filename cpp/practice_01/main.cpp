b903cb2f307c3ef2ef430a02472f7880eb3bd9cf
Hyun Jin Kim <kim.hyunjin@kaist.ac.kr>
2024-11-25 16:14:17 +0900

    Modify practice_01/main.cpp

--- cpp/practice_01/main.cpp
+++ cpp/practice_01/main.cpp
@@ -63,7 +63,13 @@ std::vector<double> readInputFile(std::string input_file)
 
 std::vector<double> scaleData(std::vector<double> vertices, double scale_factor)
 {
-    return vertices;
+    std::vector<double> scaled_vertices;
+    int number_of_data_points = static_cast<int>(vertices.size());
+    for (int i = 0; i < number_of_data_points; ++i)
+    {
+        scaled_vertices.push_back(scale_factor * vertices[i]);
+    }
+    return scaled_vertices;
 }
 
 
